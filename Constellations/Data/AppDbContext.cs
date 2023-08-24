f923024587c3c7afb54f01833806a0821eb54c74
dnk <carinefrvgv@gmail.com>
2023-08-24 19:43:05 -0300

    adding context

---
+++ Constellations/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Constellations.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Constellations.Data
+{
+    public class AppDbContext : DbContext
+    { 
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Constellation> Constellations { get; set; }
+        public DbSet<Star> Stars { get; set; }
+        public DbSet<SpectralClassification> SpectralClassifications { get; set; }
+        public DbSet<StarEvolutionaryStage> EvolutionaryStages { get; set; }
+    }
+}
